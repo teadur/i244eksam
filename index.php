97d504250ea90cff40808f76f1f68eb54487e28c
Georg Kahest <georg.kahest@itcollege.ee>
2017-06-04 17:52:36 +0300

    CSSi kasutamise näide, sisendi valideerimine

--- index.php
+++ index.php
@@ -5,18 +5,20 @@
 
 # tegeleme töötlusega ainult siis kui andmed on postitud
 if(isset($_POST['summa'])) {
-    $summa = $_POST['summa'];
-    $valuuta = $_POST['valuuta'];
+    $summa = htmlspecialchars($_POST['summa']);
+    $valuuta = htmlspecialchars($_POST['valuuta']);
 # echo "$_POST[summa] <br>";
 # echo "$_POST[valuuta]";
+
+# konverteerime vastavalt kasutaja valitule:
 if ( $valuuta == "eurusd") {
     $tulemus = $summa * 1.13;
-    echo "konverdime $summa eurodest $tulemus dollariks ";
+    echo "konverdime $summa eurot $tulemus dollariks ";
 }
 
 if ( $valuuta == "usdeur") {
      $tulemus = $summa * 0.89;
-     echo "konverdime $summa dollaritest $tulemus euroks ";
+     echo "konverdime $summa dollarit $tulemus euroks ";
     }
 
 }
@@ -24,17 +26,20 @@ if ( $valuuta == "usdeur") {
 ?>
 
 <html>
+<head>
+    <link rel="stylesheet" type="text/css" href="kujundus.css">
+</head>
 <body>
-
+<div class="keskel">
 <form action = "<?php $_PHP_SELF ?>" method = "POST">
     Summa: <input type = "text" name = "summa" />
     <p>
-    <input type="radio" name="valuuta" value="eurusd" checked>EUR to USD
-        <input type="radio" name="valuuta" value="usdeur">USD to EUR
-    <input type = "submit" />
+    <input type="radio" name="valuuta" value="eurusd" checked>EUR-USD
+        <input type="radio" name="valuuta" value="usdeur">USD-EUR
+    <input type = "submit" value="konverteeri"/>
     </p>
 </form>
-
+</div>
 
 </body>
 </html>
