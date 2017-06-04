70e844a90f52867ea51838b8992573505c807aa1
Georg Kahest <georg.kahest@itcollege.ee>
2017-06-04 17:25:36 +0300

    nõutud funktsionaalsus olemas

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php
-if(isset($_POST[summa])) {
-echo "$_POST[summa] <br>";
-echo "$_POST[valuuta]";
-}
 # Loo lihtne valuuta kalkulaator. Piisab EUR→USD ja USD→EUR võimalusest. Kurss võib olla fikseeritud.
+# 1 eur = 1.13 usd
+# 1 usd = 0.89 eur
+
+# tegeleme töötlusega ainult siis kui andmed on postitud
+if(isset($_POST['summa'])) {
+    $summa = $_POST['summa'];
+    $valuuta = $_POST['valuuta'];
+# echo "$_POST[summa] <br>";
+# echo "$_POST[valuuta]";
+if ( $valuuta == "eurusd") {
+    $tulemus = $summa * 1.13;
+    echo "konverdime $summa eurodest $tulemus dollariks ";
+}
+
+if ( $valuuta == "usdeur") {
+     $tulemus = $summa * 0.89;
+     echo "konverdime $summa dollaritest $tulemus euroks ";
+    }
+
+}
 
 ?>
 
@@ -14,7 +30,7 @@ echo "$_POST[valuuta]";
     Summa: <input type = "text" name = "summa" />
     <p>
     <input type="radio" name="valuuta" value="eurusd" checked>EUR to USD
-      <input type="radio" name="valuuta" value="usdeur">USD to EUR
+        <input type="radio" name="valuuta" value="usdeur">USD to EUR
     <input type = "submit" />
     </p>
 </form>
